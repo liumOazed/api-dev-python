ce250b4d5d8022986f8d2f205b89cad995b73583
liumOazed <oazedlium@mail.com>
2025-01-06 12:58:50 +0600

    added alembic migration

--- app/main.py
+++ app/main.py
@@ -7,17 +7,17 @@ from .config import settings
 app = FastAPI()
 
 # Create tables on app startup
-@app.on_event("startup") 
-def on_startup():
-    create_db_and_tables()
-    print("Database tables created successfully.")
+# @app.on_event("startup") 
+# def on_startup():
+#     create_db_and_tables()
+#     print("Database tables created successfully.")
 
 
 
 # Path operation 
-# @app.get("/")
-# def root(): # keep it as descriptive as possible
-#     return {"message": "Welcome to FastAPI!"}
+@app.get("/")
+def root(): # keep it as descriptive as possible
+    return {"message": "Welcome to FastAPI!"}
 
 app.include_router(post.router)
 app.include_router(user.router)
