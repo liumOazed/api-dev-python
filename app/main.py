3f1387f081354e4cf4123a9412a213a3fe3ca321
liumOazed <oazedlium@mail.com>
2025-01-07 15:23:11 +0600

    CORS added

--- app/main.py
+++ app/main.py
@@ -2,9 +2,20 @@ from fastapi import FastAPI
 from app.database import create_db_and_tables
 from .routers import post, user, auth, vote
 from .config import settings 
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Create tables on app startup
 # @app.on_event("startup") 
